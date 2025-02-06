38c1321c04dd3ad582b43534c97f49d00a3540fd
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-02-06 14:28:46 +0500

    CRUD implemented on Every Task

--- task/urls.py
+++ task/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home_view,name='home'),
-    path('all_tasks',views.tasks_list,name='all_tasks'),
-    path('create_task',views.create_task,name='task_create')
+    path('all_tasks/',views.tasks_list,name='all_tasks'),
+    path('create_task/',views.create_task,name='task_create'),
+    path('delete_task/<int:id>/',views.delete_task,name='task_delete'),
+    path('update_task/<int:id>/',views.update_task,name='task_update')
 ]
--- task/views.py
+++ task/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.urls import reverse
 from django.contrib import messages
 from .models import Task
@@ -31,3 +31,28 @@ def create_task(request):
     else:
         form = TaskForm()        
     return render(request,'task/create_task.html',{'form' : form})
+
+
+# Delete Task
+def delete_task(request,id):
+    task = get_object_or_404(Task,pk = id)
+    task.delete()
+    messages.success(request,'You Task Has Been Deleted Successfully!')
+    return redirect(reverse('all_tasks'))
+
+
+# Update Existing Task
+
+def update_task(request,id):
+    task  = get_object_or_404(Task, pk = id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Your Task Has Been Updated Successfully!')
+            return redirect(reverse('all_tasks'))
+        
+    else:
+        form = TaskForm(instance=task)
+        
+    return render(request,'task/update_task.html',{'form':form})
